7ac8483adc708c0ea156eba0008c305d4d88cc6e
Taguwa <tagucraft@gmail.com>
2022-10-14 13:02:26 +0900

    update

--- js/login_function.js
+++ js/login_function.js
@@ -1,1 +1,14 @@
-//login機能
+//login中のユーザーを識別
+
+let LOGIN_USER = NULL;
+let USER_NAME = NULL;
+
+var cookies = document.cookie; //全てのcookieを取り出して
+var cookiesArray = cookies.split(';'); // ;で分割し配列に
+
+for(var c of cookiesArray){ //一つ一つ取り出して
+    var cArray = c.split('='); //さらに=で分割して配列に
+    if( cArray[0] == 'key名'){ // 取り出したいkeyと合致したら
+        console.log(cArray);  // [key,value] 
+    }
+}
